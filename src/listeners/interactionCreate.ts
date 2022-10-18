ac13c667cc4043e5a773673a3ea5ac164ad0ec9a
gzordrai <40698142+gzordrai@users.noreply.github.com>
2022-10-19 01:25:35 +0200

    Update listener to support buttons

--- src/listeners/interactionCreate.ts
+++ src/listeners/interactionCreate.ts
@@ -1,4 +1,4 @@
-import { CommandInteraction, Client, Interaction } from "discord.js";
+import { CommandInteraction, Client, Interaction, ButtonInteraction, Role, GuildMemberRoleManager } from "discord.js";
 import { JsonDB } from "node-json-db";
 import { commands } from "../commands";
 import { ICommand } from "../ICommand";
@@ -7,6 +7,8 @@ export default (client: Client, database: JsonDB): void => {
     client.on("interactionCreate", async (interaction: Interaction) => {
         if (interaction.isCommand())
             await handleSlashCommand(client, interaction, database);
+        else if (interaction.isButton())
+            await handleButton(client, interaction, database);
     })
 }
 
@@ -20,4 +22,30 @@ const handleSlashCommand = async (client: Client, interaction: CommandInteractio
 
     await interaction.deferReply();
     slashCommand.run(client, interaction, database);
+}
+
+const handleButton = async (client: Client, interaction: ButtonInteraction, database: JsonDB): Promise<void> => {
+    let message: string = "";
+
+    if(interaction.inCachedGuild()) {
+        const role: Role = interaction.guild.roles.cache.get(interaction.customId)!;
+        const userRoles: GuildMemberRoleManager = interaction.member.roles;
+
+        message += `Le rôle ${role.name}`;
+
+        if(!userRoles.cache.get(role.id)) {
+            interaction.member.roles.add(role);
+            message += " a été ajouté avec succès !";
+        } else {
+            interaction.member.roles.remove(role);
+            message += " a été supprimé avec succès !";
+        }
+
+        if(!(await database.exists(`/${interaction.user.id}`)))
+            database.push(`/${interaction.user.id}`, { role: true, presentation: false }, true);
+        else
+            database.push(`/${interaction.user.id}/role`, true, true);
+    }
+
+    await interaction.reply({ content: message, ephemeral: true });
 }
