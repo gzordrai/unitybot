c7ebe3bf609d7e334f15c1605fd94578868a81bc
gzordrai <40698142+gzordrai@users.noreply.github.com>
2022-10-30 18:20:37 +0100

    New architecture main file

--- src/ICommand.ts
+++
@@ -1,6 +0,0 @@
-import { CommandInteraction, ChatInputApplicationCommandData, Client } from "discord.js";
-import { JsonDB } from "node-json-db";
-
-export interface ICommand extends ChatInputApplicationCommandData {
-    run: (client: Client, interaction: CommandInteraction, database: JsonDB) => void;
-} 
---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import { GatewayIntentBits } from "discord.js";
+import { config } from "dotenv";
+import { readdirSync } from "fs";
+import path from "path";
+import { ExtendedClient, ICommand, IEvent } from "./bot";
+
+config({ path: "../.env" });
+
+const client: ExtendedClient = new ExtendedClient({
+    intents: [
+        GatewayIntentBits.Guilds
+    ],
+    partials: [
+
+    ]
+});
+const commandsPath: string = path.join(__dirname, "commands");
+const eventsPath: string = path.join(__dirname, "events");
+const commandFiles: Array<string> = readdirSync(commandsPath).filter(file => file.endsWith(".js"));
+const eventFiles: Array<string> = readdirSync(eventsPath).filter(file => file.endsWith(".js"));
+
+for(const file of commandFiles) {
+    const filePath: string = path.join(commandsPath, file);
+    const command: ICommand = require(filePath).default;
+
+    client.commands.set(command.data.name, command);
+}
+
+for(const file of eventFiles) {
+    const filePath: string = path.join(eventsPath, file);
+    const event: IEvent = require(filePath).default;
+
+    if(event.once)
+        client.once(event.name, (...args) => event.execute(...args));
+    else
+        client.on(event.name, (...args) => event.execute(...args));
+}
+
+client.login(process.env.TOKEN);
