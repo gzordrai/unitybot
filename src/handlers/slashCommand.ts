d98ed31f0968af1fafcc9105ea9efe8cf99e8596
gzordrai <40698142+gzordrai@users.noreply.github.com>
2022-10-31 16:44:47 +0100

    Add log

--- src/handlers/slashCommand.ts
+++ src/handlers/slashCommand.ts
@@ -1,15 +1,20 @@
 import { CommandInteraction } from "discord.js";
 import { ExtendedClient, ICommand } from "../bot";
 
+const modalCommands: Array<string> = ["presentation"];
+
 export const handleSlashCommand = async (client: ExtendedClient, interaction: CommandInteraction): Promise<void> => {
     const command: ICommand = client.commands.get(interaction.commandName)!;
 
     try {
-        await interaction.deferReply();
-        await command.execute(client, interaction);
+        if(!modalCommands.includes(interaction.commandName)) {
+            await interaction.deferReply();
+            client.logger.info(`${interaction.user.username} executed /${interaction.commandName} command`);
+            await command.execute(client, interaction);
+        } else
+            await command.execute(client, interaction);
     } catch (error_: any) {
         const error = error_ as Error;
-
         client.logger.error(error.message);
         await interaction.reply({ content: "Une erreur inattendue s'est produite lors de l'éxecution de la commande", ephemeral: true });
     }
