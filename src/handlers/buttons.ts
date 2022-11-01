5890766ebc5b1365babf06ba138fddce7a82035f
gzordrai <40698142+gzordrai@users.noreply.github.com>
2022-11-01 16:51:40 +0100

    Add a buttons handler

---
+++ src/handlers/buttons.ts
@@ -0,0 +1,33 @@
+import { ButtonInteraction, GuildMemberRoleManager, Role } from "discord.js";
+import { ExtendedClient } from "../bot";
+
+export const handleButton = async (client: ExtendedClient, interaction: ButtonInteraction): Promise<void> => {
+    if(interaction.customId.startsWith("role-"))
+        roleButton(client, interaction);
+}
+
+const roleButton = async (client: ExtendedClient, interaction: ButtonInteraction): Promise<void> => {
+    let message: string = '-';
+
+    await interaction.deferReply();
+
+    if(interaction.inCachedGuild()) {
+        const roleId: string = interaction.customId.split('-')[1];
+        const role: Role = interaction.guild!.roles.cache.get(roleId)!;
+        const userRoles: GuildMemberRoleManager = interaction.member.roles;
+
+        message = `Le rôle ${role.name}`;
+
+        if(!userRoles.cache.get(role.id)) {
+            userRoles.add(role);
+            message += " a été ajouté avec succès !";
+        } else {
+            userRoles.remove(role);
+            message += " a été supprimé avec succès !";
+        }
+
+        client.database.push(`/${interaction.user.id}/role`, true);
+    }
+
+    await interaction.editReply({ content: message });
+}
