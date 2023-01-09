42fb62f2f68426bed8bf2c422b80227811c2a5c3
gzordrai <40698142+gzordrai@users.noreply.github.com>
2023-01-09 01:53:31 +0100

    Add ping command

---
+++ src/commands/ping.ts
@@ -0,0 +1,19 @@
+import { ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder } from "discord.js";
+import { Command } from "../bot";
+
+export const command: Command = {
+    data: new SlashCommandBuilder()
+        .setName("ping")
+        .setDescription("Information sur la latence du bot"),
+    modal: false,
+    async execute(interaction: ChatInputCommandInteraction): Promise<void> {
+        const embed: EmbedBuilder = new EmbedBuilder();
+
+        embed.setColor("Blue");
+        embed.setTitle(`Roundtrip latency: ${Date.now() - interaction.createdTimestamp}ms`);
+
+        await interaction.followUp({ embeds: [embed] });
+    }
+}
+
+export default command;
