3e7bf5d83ff0471b9592549b61bdc0afc2dcc66a
gzordrai <40698142+gzordrai@users.noreply.github.com>
2023-03-21 18:30:57 +0100

    Fix undeferred interaction error

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,6 +1,6 @@
-import { Events, Interaction, Role } from "discord.js";
+import { ButtonInteraction, Events, Interaction, ModalSubmitInteraction, Role } from "discord.js";
 import { Database, User } from "../database";
-import { ExtendedClient, Event } from "../bot";
+import { ExtendedClient, Event, Command } from "../bot";
 import { handleButton, handleModal, handleSlashCommand } from "../handlers";
 
 const event: Event = {
@@ -10,30 +10,34 @@ const event: Event = {
         const userId: string = interaction.user.id;
         let user: User;
 
-        if (interaction.isChatInputCommand()) {
-            if (!client.commands.get(interaction.commandName)!.modal)
-                await interaction.deferReply();
-        }
-
-        if (await Database.has(userId))
-            user = await Database.getUser(userId);
-        else
-            user = await Database.addUser(userId);
-
-        if (interaction.isChatInputCommand()) {
-            await handleSlashCommand(client, interaction);
-        } else if (interaction.isButton()) {
-            await interaction.deferReply({ ephemeral: true });
-            await handleButton(interaction, user);
-        } else if (interaction.isModalSubmit()) {
-            await interaction.deferReply();
-            await handleModal(interaction, user);
-        }
-
         if (interaction.inCachedGuild()) {
-            if ((await Database.getUser(userId)).isEligible()) {
-                const role: Role = interaction.guild.roles.cache.get(process.env.MEMBER_ROLE_ID!)!;
-                await interaction.member?.roles.add(role);
+            if (!interaction.isChatInputCommand()) {
+                if (interaction.isButton())
+                    await interaction.deferReply({ ephemeral: true });
+                else if (interaction.isModalSubmit())
+                    await interaction.deferReply();
+
+                if (await Database.has(userId))
+                    user = await Database.getUser(userId);
+                else
+                    user = await Database.addUser(userId);
+
+                if (interaction.isButton())
+                    await handleButton(interaction, user);
+                else if (interaction.isModalSubmit())
+                    await handleModal(interaction, user);
+
+                if ((await Database.getUser(userId)).isEligible()) {
+                    const role: Role = interaction.guild.roles.cache.get(process.env.MEMBER_ROLE_ID!)!;
+                    await interaction.member?.roles.add(role);
+                }
+            } else {
+                const command: Command = client.commands.get(interaction.commandName)!;
+
+                if (!command.modal)
+                    await interaction.deferReply();
+
+                await handleSlashCommand(client, interaction);
             }
         }
     },
