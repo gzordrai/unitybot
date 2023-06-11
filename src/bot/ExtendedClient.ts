bef318bb2c727110a17a68dd3c6cbd6cc03729d6
gzordrai <40698142+gzordrai@users.noreply.github.com>
2023-06-11 16:53:58 +0200

    Addition of a collection to manage pending users

--- src/bot/ExtendedClient.ts
+++ src/bot/ExtendedClient.ts
@@ -1,6 +1,8 @@
 import { Client, Collection } from "discord.js";
 import { Command } from "./Command";
+import { User } from "../models";
 
 export class ExtendedClient extends Client {
     public commands: Collection<string, Command> = new Collection<string, Command>();
+    public pendingUsers: Collection<string, User> = new Collection<string, User>();
 }
