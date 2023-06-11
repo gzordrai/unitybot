1bcad273370c28337dd4d659530c30b69743f608
gzordrai <40698142+gzordrai@users.noreply.github.com>
2023-06-11 18:15:01 +0200

    Rework interactionCreate event

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,5 +1,5 @@
 import { Events, Interaction, Role } from "discord.js";
-import { Database, User } from "../database";
+import { User } from "../models";
 import { ExtendedClient, Event, Command } from "../bot";
 import { handleButton, handleModal, handleSlashCommand } from "../handlers";
 
@@ -8,55 +8,43 @@ const event: Event = {
     once: false,
     async execute(client: ExtendedClient, interaction: Interaction): Promise<void> {
         const userId: string = interaction.user.id;
-        let user: User;
 
-        if (interaction.isButton()) {
-            let enterPoint = (Date.now() - interaction.createdAt.getTime()) / 1000;
-            console.log(`[${new Date()}] Button by ${interaction.user.username}, enter point: ` + enterPoint);
-
-            try {
-                await interaction.deferReply({ ephemeral: true });
-            } catch (err) {
-                console.log(err);
-            }
-
-            console.log(`[${new Date()}] Button by ${interaction.user.username}, total time: ` + (((Date.now() - interaction.createdAt.getTime()) / 1000) - enterPoint));
-        } else if (interaction.isModalSubmit()) {
-            let enterPoint = (Date.now() - interaction.createdAt.getTime()) / 1000;
-            console.log(`[${new Date()}] Modal submit by ${interaction.user.username}, enter point: ` + enterPoint);
-    
-            try {
-                await interaction.deferReply();
-            } catch (err) {
-                console.log(err);
-            }
-
-            console.log(`[${new Date()}] Modal submit by ${interaction.user.username}, total time: ` + (((Date.now() - interaction.createdAt.getTime()) / 1000) - enterPoint));
-        } else if (interaction.isChatInputCommand()) {
+        if (interaction.isChatInputCommand()) {
             const command: Command = client.commands.get(interaction.commandName)!;
 
-            if (!command.modal) {
-                console.log(`Command by ${interaction.user.username}: ` + (Date.now() - interaction.createdAt.getTime()) / 1000);
-                await interaction.deferReply();
-            }
+                if (!command.modal)
+                    await interaction.deferReply({ ephemeral: command.ephemeral });
+        } else if (interaction.isButton())
+            await interaction.deferReply({ ephemeral: true });
+        else if (interaction.isModalSubmit())
+            await interaction.deferReply({ ephemeral: false });
 
-            await handleSlashCommand(client, interaction);
-        }
+        if (!client.pendingUsers.has(interaction.user.id))
+            client.pendingUsers.set(userId, new User(userId));
+
+        const user: User = client.pendingUsers.get(userId)!;
 
         if (interaction.inCachedGuild()) {
-            if (await Database.has(userId))
-                user = await Database.getUser(userId);
-            else
-                user = await Database.addUser(userId);
-
-            if (interaction.isButton())
-                await handleButton(interaction, user);
-            else if (interaction.isModalSubmit())
-                await handleModal(interaction, user);
-
-            if ((await Database.getUser(userId)).isEligible()) {
-                const role: Role = interaction.guild.roles.cache.get(process.env.MEMBER_ROLE_ID!)!;
-                await interaction.member?.roles.add(role);
+            try {
+                if (interaction.isButton()) {
+                    await handleButton(interaction);
+    
+                    user.setRoleStatus(true);
+                } else if (interaction.isModalSubmit()) {
+                    await handleModal(interaction);
+    
+                    user.setPresentationStatus(true);
+                } else if (interaction.isChatInputCommand())
+                    await handleSlashCommand(client, interaction);
+    
+                if (user.isEligible()) {
+                    const role: Role = interaction.guild.roles.cache.get(process.env.MEMBER_ROLE_ID!)!;
+    
+                    interaction.member?.roles.add(role);
+                    client.pendingUsers.delete(userId);
+                }
+            } catch (err: unknown) {
+                console.log(err);
             }
         }
     },
