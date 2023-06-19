21bb0568a3e32e81b8f30cddd2c49c7fe6582da1
gzordrai <40698142+gzordrai@users.noreply.github.com>
2023-06-20 00:58:25 +0200

    Remove slash command handler

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,7 +1,7 @@
 import { Events, Interaction, Role } from "discord.js";
 import { User } from "../models";
 import { ExtendedClient, Event, Command } from "../bot";
-import { handleButton, handleModal, handleSlashCommand } from "../handlers";
+import { handleButton, handleModal } from "../handlers";
 
 const event: Event = {
     name: Events.InteractionCreate,
@@ -9,15 +9,25 @@ const event: Event = {
     async execute(client: ExtendedClient, interaction: Interaction): Promise<void> {
         const userId: string = interaction.user.id;
 
-        if (interaction.isChatInputCommand()) {
-            const command: Command = client.commands.get(interaction.commandName)!;
-
-                if (!command.modal)
-                    await interaction.deferReply({ ephemeral: command.ephemeral });
-        } else if (interaction.isButton())
+        if (interaction.isButton()) {
+            console.log(`Button => ${((Date.now() - interaction.createdAt.getTime()) / 1000)}`);
             await interaction.deferReply({ ephemeral: true });
-        else if (interaction.isModalSubmit())
+        } else if (interaction.isModalSubmit()) {
+            console.log(`Modal => ${((Date.now() - interaction.createdAt.getTime()) / 1000)}`);
             await interaction.deferReply({ ephemeral: false });
+        } else if (interaction.isChatInputCommand()) {
+        const command: Command = client.commands.get(interaction.commandName)!;
+
+            if (!command.modal)
+                await interaction.deferReply({ ephemeral: command.ephemeral });
+
+            try {
+                await command.execute(interaction);
+            } catch (error) {
+                console.log(error);
+                await interaction.followUp({ content: "Une erreur inattendue s'est produite lors de l'éxecution de la commande", ephemeral: true });
+            }
+        }
 
         if (!client.pendingUsers.has(interaction.user.id))
             client.pendingUsers.set(userId, new User(userId));
@@ -28,18 +38,17 @@ const event: Event = {
             try {
                 if (interaction.isButton()) {
                     await handleButton(interaction);
-    
+
                     user.setRoleStatus(true);
                 } else if (interaction.isModalSubmit()) {
                     await handleModal(interaction);
-    
+
                     user.setPresentationStatus(true);
-                } else if (interaction.isChatInputCommand())
-                    await handleSlashCommand(client, interaction);
-    
+                }
+
                 if (user.isEligible()) {
                     const role: Role = interaction.guild.roles.cache.get(process.env.MEMBER_ROLE_ID!)!;
-    
+
                     await interaction.member?.roles.add(role);
                     client.pendingUsers.delete(userId);
                 }
