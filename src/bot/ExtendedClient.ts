9778e8f7dc2298b9d5b129d51ac895869987e589
gzordrai <40698142+gzordrai@users.noreply.github.com>
2023-07-03 21:39:41 +0200

    Added dynamic import to optimize perfs

--- src/bot/ExtendedClient.ts
+++ src/bot/ExtendedClient.ts
@@ -1,8 +1,38 @@
 import { Client, Collection } from "discord.js";
 import { Command } from "./Command";
 import { User } from "../models";
+import path from "path";
+import { readdirSync } from "fs";
+import { Event } from "./Event";
 
 export class ExtendedClient extends Client {
     public commands: Collection<string, Command> = new Collection<string, Command>();
     public pendingUsers: Collection<string, User> = new Collection<string, User>();
+
+    public async loadCommands(): Promise<void> {
+        const commandsPath = path.join(__dirname, "../commands");
+        const commandFiles: Array<string> = readdirSync(commandsPath).filter(file => file.endsWith(".js"));
+
+        for (const file of commandFiles) {
+            const filePath: string = path.join(commandsPath, file);
+            const { default: command } = await import(filePath) as { default: Command };
+    
+            this.commands.set(command.data.name, command);
+        }
+    }
+
+    public async loadEvents(): Promise<void> {
+        const eventsPath: string = path.join(__dirname, "../events");
+        const eventFiles: Array<string> = readdirSync(eventsPath).filter(file => file.endsWith(".js"));
+
+        for (const file of eventFiles) {
+            const filePath: string = path.join(eventsPath, file);
+            const { default: event } = await import(filePath) as { default: Event };
+        
+            if (event.once)
+                this.once(event.name, (...args) => event.execute(this, ...args));
+            else
+            this.on(event.name, (...args) => event.execute(this, ...args));
+        }
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import { GatewayIntentBits } from "discord.js";
 import { config } from "dotenv";
-import { readdirSync } from "fs";
 import path from "path";
-import { ExtendedClient, Command, Event } from "./bot";
+import { ExtendedClient } from "./bot";
 
 config({ path: path.join(__dirname, "../.env") });
 
@@ -12,26 +11,10 @@ const client: ExtendedClient = new ExtendedClient({
     ],
     partials: []
 });
-const commandsPath: string = path.join(__dirname, "commands");
-const eventsPath: string = path.join(__dirname, "events");
-const commandFiles: Array<string> = readdirSync(commandsPath).filter(file => file.endsWith(".js"));
-const eventFiles: Array<string> = readdirSync(eventsPath).filter(file => file.endsWith(".js"));
 
-for (const file of commandFiles) {
-    const filePath: string = path.join(commandsPath, file);
-    const command: Command = require(filePath).default;
+(async () => {
+    await client.loadCommands();
+    await client.loadEvents();
 
-    client.commands.set(command.data.name, command);
-}
-
-for (const file of eventFiles) {
-    const filePath: string = path.join(eventsPath, file);
-    const event: Event = require(filePath).default;
-
-    if (event.once)
-        client.once(event.name, (...args) => event.execute(client, ...args));
-    else
-        client.on(event.name, (...args) => event.execute(client, ...args));
-}
-
-client.login(process.env.TOKEN);
+    client.login(process.env.TOKEN);
+})()
