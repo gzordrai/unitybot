f26284a9cab62437380a20b3847f080a5c159861
gzordrai <gzordrai@users.noreply.github.com>
2024-05-28 23:08:47 +0200

    Remove old bot architecture

--- src/bot/Command.ts
+++
@@ -1,8 +0,0 @@
-import { ChatInputCommandInteraction, CommandInteraction, SlashCommandBuilder, SlashCommandSubcommandsOnlyBuilder } from "discord.js";
-
-export interface Command {
-    data: SlashCommandBuilder | Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup"> | SlashCommandSubcommandsOnlyBuilder;
-    ephemeral: boolean;
-    modal: boolean;
-    execute: (interaction: ChatInputCommandInteraction) => Promise<void>;
-}
